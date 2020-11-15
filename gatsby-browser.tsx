fc79c3e7844b8491f2f452f06a173daa0518a5d9
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-11-14 23:11:04 -0600

    Merge branch 'main' of github.com:crfroehlich/blog into component-refactor

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -11,12 +11,6 @@ export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
 
-export const wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>;
-};
-
 // wrap the app with redux provider
 export const wrapRootElement: React.FC<any> = ({ element }) => {
   const store = configureStore();
