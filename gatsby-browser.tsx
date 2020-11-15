c53886a0907d89421204fbe2ebce832d1a22fb2e
Christopher Froehlich <crf@luddites.me>
2020-11-14 22:15:04 -0500

    rebase

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -17,6 +17,12 @@ export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
 
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <Layout {...props}>{element}</Layout>;
+};
+
 // wrap the app with redux provider
 export const wrapRootElement: React.FC<any> = ({ element }) => {
   const store = configureStore();
